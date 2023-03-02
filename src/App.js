4fa593a76dd4283cfd6776621429993b029b1ec1
haleytoye <hjtoye@gmail.com>
2023-03-02 12:34:30 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello World! </p>
+        <Weather city="Toronto" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
