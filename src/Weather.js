bb43f619584631019d0521ab0027151f06990b0f
haleytoye <hjtoye@gmail.com>
2023-03-03 18:56:59 -0500

    updated css

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Audio } from "react-loader-spinner";
 import "./Weather.css";
 
 export default function Weather() {
@@ -8,13 +7,15 @@ export default function Weather() {
   const [loaded, setLoaded] = useState(false);
   const [weather, setWeather] = useState({});
 
+  let tmpCity = "";
+
   function displayWeather(response) {
     setLoaded(true);
     setWeather({
       temp: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/{response.data.weather[0].icon}@2x.png`,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
     });
   }
@@ -22,17 +23,27 @@ export default function Weather() {
   function handleSubmit(event) {
     event.preventDefault();
     let apiKey = "094780c710fa4efd669f0df8c3991927";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${tmpCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
+
+    setCity(tmpCity);
+    tmpCity = "";
   }
 
   function updateCity(event) {
-    setCity(event.target.value);
+    event.preventDefault();
+    //setCity(event.target.value);
+    tmpCity = event.target.value;
   }
 
   let form = (
     <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <input
+        type="search"
+        className="seach-bar"
+        placeholder="Enter a city.."
+        onChange={updateCity}
+      />
       <button type="Submit">Search</button>
     </form>
   );
@@ -40,26 +51,22 @@ export default function Weather() {
   if (loaded) {
     return (
       <div>
-        <div>
+        <p>Weather Search: {city} </p>
+        <div className="weather">
           {form}
           <ul>
             <li>Temperature: {Math.round(weather.temp)}°C</li>
             <li>
               Description: <span className="desc">{weather.description}</span>
             </li>
-            <li>Wind: {weather.wind}km/h</li>
+            <li>Wind: {Math.round(weather.wind)} km/h</li>
             <li>Humidity: {weather.humidity}%</li>
+            <li>
+              {" "}
+              <img src={weather.icon} alt={weather.description} />
+            </li>
           </ul>
         </div>
-        <Audio
-          height="80"
-          width="80"
-          radius="9"
-          color="yellow"
-          ariaLabel="loading"
-          wrapperStyle
-          wrapperClass
-        />
       </div>
     );
   } else {
